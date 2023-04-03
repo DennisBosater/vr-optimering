3e498ef78cb07f97da6f12a7454d39bb0266f3ef
DennisBosater <113235063+DennisBosater@users.noreply.github.com>
2023-04-03 17:34:41 +0200

    Title added fixed minor glitches

--- Assets/VR-utveckling-mobil-plattform-Dennis/Scripts/batdeath.cs
+++ Assets/VR-utveckling-mobil-plattform-Dennis/Scripts/batdeath.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class batdeath : MonoBehaviour
 {
-    public GameObject particleEffectPrefab;
     public int scoreValue = 1;
 
     private void OnDestroy()
     {
-        if (particleEffectPrefab != null)
-        {
-            Instantiate(particleEffectPrefab, transform.position, Quaternion.identity);
             UICollectables.instance.ChangeScore(scoreValue);
-        }
     }
 }
